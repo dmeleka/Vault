0bb9724d6b0d935c760a26e686151fb89a7b4587
Daniel Meleka <daniel.meleka@student.guc.edu.eg>
2023-09-08 15:45:54 +0300

    implemented setFavourite, deleteItem, updateItem

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -74,3 +74,92 @@ export const addCard = async (req, res) => {
         res.status(404).json({ message: error.message });
     }
 }
+
+export const setFavourite = async (req, res) => {
+    try {
+        const filter = { _id: req.userId, "logins._id": req.body.itmId };
+
+        const user = await User.findOne(filter);
+
+        if (user) {
+            const loginIndex = user.logins.findIndex(login => login._id.toString() === req.body.itmId);
+
+            if (loginIndex !== -1) {
+                const currentFavourite = user.logins[loginIndex].favourite;
+
+                await User.updateOne(filter, { $set: { [`logins.${loginIndex}.favourite`]: !currentFavourite } });
+
+                // Now the favourite status has been toggled in the database
+                res.send("Done")
+            } else {
+                // Login item not found
+            }
+        } else {
+            // User not found
+        }
+
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const deleteItm = async (req, res) => {
+    try {
+        const itemId = req.body.itmId;
+        const user = await User.findById(req.userId);
+
+        // Search for the item across all arrays
+        const arraysToSearch = ['logins', 'cards', 'secNotes'];
+        let foundItem = null;
+        let foundArrayType = null;
+
+        for (const arrayType of arraysToSearch) {
+            const targetArray = user[arrayType];
+            const itemIndex = targetArray.findIndex(item => item._id.toString() === itemId);
+            if (itemIndex !== -1) {
+                foundItem = targetArray[itemIndex];
+                foundArrayType = arrayType;
+                targetArray.splice(itemIndex, 1);
+                break;
+            }
+        }
+
+        if (!foundItem) {
+            return res.status(404).json({ message: 'Item not found.' });
+        }
+
+        // Save the updated user
+        await user.save();
+
+        res.status(200).json({ message: `Item deleted from ${foundArrayType} successfully.` });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const updateItem = async (req, res) => {
+    try {
+        const encryptedData = encryptData(req);
+        const user = await User.findById(req.userId);
+
+        if (req.body.itmType === "L") {
+            // Use Mongoose's array filtering to find the login by _id
+            const foundLogin = user.logins.id(req.body.itmId);
+            // Update the attributes
+            foundLogin.title = encryptedData.title;
+            foundLogin.username = encryptedData.username;
+            foundLogin.password = encryptedData.password;
+            foundLogin.websiteLink = req.body.websiteLink;
+        }
+
+        // Save the updated user document
+        await user.save();
+
+        res.status(200).send("Data Updated")
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+};
