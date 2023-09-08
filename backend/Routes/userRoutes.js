f3fef36927067e7f8ac32cd26981db16b36184f9
Daniel Meleka <daniel.meleka@student.guc.edu.eg>
2023-09-08 15:46:32 +0300

    added setFavourite, deleteItem, UpdateItem routes

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Register, addLogin, addCard, getUserData } from "../Controllers/userController.js";
+import { Register, addLogin, addCard, getUserData, setFavourite, deleteItm, updateItem } from "../Controllers/userController.js";
 import { login, logout, verifyJWT } from "../Controllers/auth.js";
 
 const router = express.Router();
@@ -8,6 +8,9 @@ router.post('/register', Register);
 router.post('/login', login);
 router.post('/addLogin', verifyJWT, addLogin);
 router.post('/addCard', verifyJWT, addCard);
+router.post('/setFavourite', verifyJWT, setFavourite);
+router.post('/deleteItm', verifyJWT, deleteItm);
+router.post('/updateItem', verifyJWT, updateItem);
 
 router.get('/getUserData', verifyJWT, getUserData);
 
